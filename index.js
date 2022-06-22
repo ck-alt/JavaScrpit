f0db82499f4d12f8fa59aa5ebea046260e269682
ChukwuKingsley <79272441+ck-alt@users.noreply.github.com>
2022-06-22 15:08:50 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let el = document.createElement("p")
+el.textContent = "Name: Chukwu Kingsley" + " " + "Height: 5 feet 9 inches" + " " + "Country: Nigeria"
+let screen = document.getElementById("screen")
+screen.appendChild(el)
